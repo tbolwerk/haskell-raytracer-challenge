44f6e7525b25b11b587f2a52e6be627cf1e913d0
twanbolwerk <twanbolwerk@gmail.com>
2021-07-07 23:03:09 +0200

    determinant working

--- RayTracer/src/Matrices.hs
+++ RayTracer/src/Matrices.hs
@@ -17,9 +17,14 @@ instance Eq (Matrix Double) where
 
 instance (Num a, Fractional a) => Num (Matrix a) where
    a * b = listToMatrix [ celCalc a b i j | i <- [0..getNRows a-1], j <- [0..getNCols a-1]]
+    where check = (snd . snd . bounds . getArray)
 
 instance Functor Matrix where
     fmap f m = listToMatrix (map f (elems $ getArray m))
+
+instance Applicative Matrix where
+    pure x = matrix 4 4 (\_ -> x)
+    (<*>) = undefined
 
 celCalc :: (Num a, Fractional a) => Matrix a -> Matrix a -> Int -> Int -> a
 celCalc a b i j = sum (zipWith (*) (getRow a i 0) (getCol b j 0))
@@ -116,18 +121,47 @@ identityMatrix = matrix 4 4 (\(i,j) -> if i == j then 1.0 else 0.0)
 transpose :: Matrix a -> Matrix a 
 transpose m = matrix (getNRows m) (getNCols m) (\(i,j) -> getArray m ! (j,i))
 
-determinant :: (Show a, Num a) => Matrix a -> a
+determinant :: (Show a, Num a, Fractional a) => Matrix a -> a
 determinant (Matrix 2 2 array) = (a * d) - (b * c)
     where a = array ! (0,0)
           b = array ! (0,1)
           c = array ! (1,0)
           d = array ! (1,1)
+determinant m@(Matrix 3 3 array) = a * efhi + b * dfgi + c * degh 
+    where efhi = cofactor m 0 0
+          dfgi = cofactor m 0 1
+          degh = cofactor m 0 2
+          a = array ! (0,0)
+          b = array ! (1,0)
+          c = array ! (2,0)
+determinant m@(Matrix 4 4 array) = a * a0 + b * a1 + c * a2 + d * a3
+   where a0 = cofactor m 0 0
+         a1 = cofactor m 0 1
+         a2 = cofactor m 0 2
+         a3 = cofactor m 0 3
+         a = array ! (0,0)
+         b = array ! (1,0)
+         c = array ! (2,0)
+         d = array ! (3,0)
 
 submatrix :: (Num a, Show a) => Matrix a -> Row -> Column -> Matrix a
-submatrix m r c = listToMatrix $ concat (map (\x -> take c x ++ drop (1 + c) x) removeRow)
+submatrix m r c = listToMatrix $ concatMap (\x -> take c x ++ drop (1 + c) x) removeRow
  where removeRow = take r (matrixList m) ++ drop (1 + r) (matrixList m)
 
-minor :: (Num a,Show a) => Matrix a -> Row -> Column -> a
-minor m@(Matrix 3 3 array) r c = determinant (submatrix m r c)
+minor :: (Num a,Show a, Fractional a) => Matrix a -> Row -> Column -> a
+minor m r c = determinant (submatrix m r c)
 
 b = listToMatrix [3,5,0,2,-1,-7,6,-1,5]
+
+cofactor :: (Num a, Show a, Fractional a)=> Matrix a -> Row -> Column -> a
+cofactor m r c | even (r + c) = minor m r c 
+                                  | otherwise = negate (minor m r c) 
+
+a0 :: Matrix Double
+a0 = listToMatrix [1,2,6,-5,8,-4,2,6,4]
+
+matrixScalarMultiply :: (Num a,Fractional a) => Matrix a -> a -> Matrix a
+matrixScalarMultiply m s = Matrix (getNRows m) (getNCols m) (accum (\e a -> a e) (getArray m) [((i,j), (*s)) | j <- [0..getNCols m -1], i <- [0..getNRows m -1]])
+
+a10 :: Matrix Double
+a10 = listToMatrix [-2,-8,3,5,-3,1,7,3,1,2,-9,6,-6,7,7,-9]
